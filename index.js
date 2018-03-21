7cab79e9a6814872bd2265ba52282393a8ae7e87
Aaron Langley <aaronlangley@wsip-184-188-122-178.oc.oc.cox.net>
2018-03-20 17:19:58 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const ParseError = require('parse-error');//parses error so you can read error message and handle them accordingly
+module.exports.ParseError = ParseError;
+
+const CatchError = function(promise) {//global function that will help use handle promise rejections, this article talks about it http://blog.grossman.io/how-to-write-async-await-without-try-catch-blocks-in-javascript/
+    return promise
+        .then(data => {
+        return [null, data];
+}).catch(err =>
+    [pe(err)]
+);
+};
+module.exports.CatchError = CatchError;
+
+const ThrowError = function(err_message, log){ // TE stands for Throw Error
+    if(log === true){
+        console.error(err_message);
+    }
+
+    throw new Error(err_message);
+};
+module.exports.ThrowError = ThrowError;
+
+const ErrorResponse = function(res, err, code){ // Error Web Response
+    if(typeof err === 'object' && typeof err.message !== 'undefined'){
+        err = err.message;
+    }
+
+    if(typeof code !== 'undefined') res.statusCode = code;
+
+    return res.json({success:false, error: err});
+};
+module.exports.ErrorResponse = ErrorResponse;
+
+const SuccessResponse = function(res, data, code){ // Success Web Response
+    let send_data = {success:true};
+
+    if(typeof data === 'object'){
+        send_data = Object.assign(data, send_data);//merge the objects
+    }
+
+    if(typeof code !== 'undefined') res.statusCode = code;
+
+    return res.json(send_data)
+};
+module.exports.SuccessResponse = SuccessResponse;
